24f22f98566a2a022e33eb6f9af348c552489f1c
Vinicius Fidelis <160151701+fidelissss@users.noreply.github.com>
2024-09-20 12:06:03 -0300

    Kickoff-nutricionista

---
+++ principal.js
@@ -0,0 +1,13 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+paciente.querySelector("info-peso");
+
+var peso = tdPeso.textContent;
+
+vartdAltura= paciente.querySelector(".info-altura");
+
+console.log(paciente);//tr
+console.log(tdPeso);// td que tem o peso
+console.log(peso);// Obter 100
