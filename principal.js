75feb5d27a16da9e9dfad7c4df3cf1a086824767
Vinicius Fidelis <160151701+fidelissss@users.noreply.github.com>
2024-10-25 12:23:36 -0300

    Atualizacao_estilo

--- principal.js
+++ principal.js
@@ -1,23 +1,11 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-paciente.querySelector("info-peso");
-vartdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector("info-imc");
-
-var peso = tdPeso.textContent;
-
-
-console.log(paciente);//tr
-
-var imc = peso / (altura * altura);//100/2.0=100 / 4  =>>>>> 25
-tdImc.textContent = imc.toFixed(2);
-
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
 
+}
     var paciente = pacientes[i];
 
     var tdPeso = paciente.querySelector(".info-peso");
@@ -43,19 +31,11 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = "Altura inválida";
     }
 
-  if (alturaEhValida && pesoEhValido) {
+    if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
-        tdImc.textContent = imc;
+        tdImc.textContent = imc.toFixed(2);
     }
-}
+    
 
+    
 
-console.log(tdPeso);// td que tem o peso
-console.log(peso);// Obter 100
-
-if (peso < 0 || peso >= 1000) {
-    console.log("erro,peso invalido!");
-}
-if (altura < 0 || altura >= 3) {
-    console.log("erro,altura invalida!");
-}
